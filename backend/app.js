f968813e03c83b7fefc9d6a5ad29aff93c666a25
SALV <a01707122@tec.mx>
2025-03-08 14:15:37 -0600

    [ADD] express static folder

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const path = require("path");
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../frontend/views"));
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(session({
-  secret : 'mi string secreto que debe ser un string aleatorio muy largo, no como este lolxd',
-  resave: false, 
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret:
+      "mi string secreto que debe ser un string aleatorio muy largo, no como este lolxd",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const middleware_auth = (request, response, next) => {
   console.log("User not signed in");
@@ -28,5 +32,6 @@ app.use("/log_in", login_routes);
 
 app.use("/", middleware_auth, general_routes);
 
-console.log("App started in: http://localhost:3000");
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("App started in: http://localhost:3000");
+});
