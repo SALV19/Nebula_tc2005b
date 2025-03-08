e10b2828f07f02f72d96b33769edec4a7ed2eedf
SALV <a01707122@tec.mx>
2025-03-08 15:55:22 -0600

    [ADD] front-end login page / bstrap-icons / static-files

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const path = require("path");
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../frontend/views"));
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(session({
-  secret : 'mi string secreto que debe ser un string aleatorio muy largo, no como este lolxd',
-  resave: false, 
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret:
+      "mi string secreto que debe ser un string aleatorio muy largo, no como este lolxd",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const middleware_auth = (request, response, next) => {
   console.log("User not signed in");
@@ -25,8 +29,8 @@ const login_routes = require("./routes/login.routes");
 const general_routes = require("./routes/general.routes");
 
 app.use("/log_in", login_routes);
-
 app.use("/", middleware_auth, general_routes);
 
-console.log("App started in: http://localhost:3000");
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("App started in: http://localhost:3000");
+});
