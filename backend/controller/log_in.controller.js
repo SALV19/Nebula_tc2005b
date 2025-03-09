0e34c3b56fec8bc29ed1e977ac7dd2578a7eeae7
SALV <a01707122@tec.mx>
2025-03-08 19:52:23 -0600

    [ADD] minor changes : montserrat font / user.models file, clean log_in.controller

--- backend/controller/log_in.controller.js
+++ backend/controller/log_in.controller.js
@@ -1,5 +1,3 @@
-const User = require("../models/log_in.model");
-
 exports.get_log_in = (request, response) => {
   response.render("log_in");
 };
@@ -9,15 +7,5 @@ exports.post_log_in = (request, response) => {
   request.session.password = request.body.password;
   console.log(request.body.username);
   console.log(request.body.password);
-  const user = new User(request.body.username, request.body.password);
-    user.save()
-        .then(() => {
-            console.log("User saved");
-            response.redirect('/');
-        })
-        .catch((error) => {
-            console.log(error);
-            //response.redirect('/log_in');
-        }); 
 };
 
