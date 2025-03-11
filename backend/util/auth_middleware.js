ff4cab1e7a0ba78940041d886d77aebc35f21f9c
SALV <a01707122@tec.mx>
2025-03-11 11:23:20 -0600

    [ADD] auth_middleware file util

---
+++ backend/util/auth_middleware.js
@@ -0,0 +1,9 @@
+module.exports = (request, response, next) => {
+  // TO-DELETE
+  console.log("auth middleware");
+  if (request.session.user || request.user) {
+    next();
+  } else {
+    response.redirect("/log_in");
+  }
+};
