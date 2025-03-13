2556adaeb473d4161b1021932154a1a77c61424f
SALV <a01707122@tec.mx>
2025-03-13 13:09:26 -0600

    [FIX] get_user_permissions format

--- backend/sql/get_user_permissions.sql
+++ backend/sql/get_user_permissions.sql
@@ -1,4 +1,11 @@
 -- SQLBook: Code
+INSERT INTO nebula.departamento (id_departamento, id_empresa, nombre_departamento) VALUES(1, 1, 'Nuclea');
+INSERT INTO nebula.rol (id_rol, tipo_rol) VALUES(1, 'Lider');
+INSERT INTO nebula.permisos(nombre_permiso, descripcion) VALUES('view_collabs', 'ver colaboradores del equipo');
+INSERT INTO nebula.rol_permisos(nombre_permiso, id_rol) VALUES('view_collabs', 1);
+INSERT INTO nebula.equipo(id_colaborador, id_departamento, id_rol) VALUES('7bfc173a-002b-11f0-9aa8-5811223b90cc', 1, 1);
+
+-- SQLBook: Code
 SELECT nombre_permiso
   FROM colaborador c
   JOIN equipo e ON e.id_colaborador = c.id_colaborador
@@ -8,8 +15,16 @@ SELECT nombre_permiso
 -- SQLBook: Code
 INSERT INTO nebula.empresa(id_empresa, nombre_empresa)VALUES (1, 'Fixtec');
 -- SQLBook: Code
-INSERT INTO nebula.departamento (id_departamento, id_empresa, nombre_departamento) VALUES(1, 1, 'Nuclea');
-INSERT INTO nebula.rol (id_rol, tipo_rol) VALUES(1, 'Lider');
-INSERT INTO nebula.permisos(nombre_permiso, descripcion) VALUES('view_collabs', 'ver colaboradores del equipo');
-INSERT INTO nebula.rol_permisos(nombre_permiso, id_rol) VALUES('view_collabs', 1);
-INSERT INTO nebula.equipo(id_colaborador, id_departamento, id_rol) VALUES('7bfc173a-002b-11f0-9aa8-5811223b90cc', 1, 1);
+SELECT nombre_permiso
+  FROM colaborador c
+  JOIN equipo e ON e.id_colaborador = c.id_colaborador
+  JOIN rol r ON r.id_rol = e.id_rol
+  JOIN rol_permisos rp ON rp.id_rol = r.id_rol
+  WHERE c.email = (?);
+-- SQLBook: Code
+SELECT nombre_permiso
+  FROM colaborador c
+  JOIN equipo e ON e.id_colaborador = c.id_colaborador
+  JOIN rol r ON r.id_rol = e.id_rol
+  JOIN rol_permisos rp ON rp.id_rol = r.id_rol
+  WHERE c.email = (?);
