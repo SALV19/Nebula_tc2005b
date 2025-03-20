ae09d9c42e57fb8e9c6dc59f2212aebf9b28b24c
Ale Arredondo <alejandrarredondo09@gmail.com>
2025-03-20 16:08:32 -0600

    [ADD] radar graph

--- backend/controller/follow_ups.controller.js
+++ backend/controller/follow_ups.controller.js
@@ -1,5 +1,6 @@
 const Collab = require('../models/collabs.model');
-const QuestionsFollow = require('../models/follow_ups_questions.models');
+const QuestionsFollow = require('../models/follow_ups_questions.model');
+const Indicator = require('../models/indicators.model');
 
 let settings = {
   selectedOption: 'active',
@@ -8,26 +9,31 @@ let settings = {
 exports.get_requests = async (request, response) => {
   try {
     // Ejecuta ambas consultas en paralelo y espera sus resultados
-    const [collabsData, questionsData] = await Promise.all([
+    const [collabsData, questionsData, indicatorsData] = await Promise.all([
       Collab.fetchAllCompleteName(),
-      QuestionsFollow.fetchAllQuestions()
+      QuestionsFollow.fetchAllQuestions(),
+      Indicator.fetchAllindicators(),
     ]);
 
     // Extrae las filas de cada consulta
     const [rows, fieldData] = collabsData;
     const [rows_ques, fieldData_ques] = questionsData;
+    const [rows_indi, fieldData_indi] = indicatorsData;
 
     // console.log("Datos de la tabla de colaboradores:", fieldData);
     console.log("Colaboradores:", rows);
     // console.log("Datos de la tabla de preguntas:", fieldData_ques);
     console.log("Preguntas:", rows_ques);
+    console.log("indicadores:", rows_indi);
+
 
     response.render("register_followUp", {
       ...settings,
       permissions: request.session.permissions,
       csrfToken: request.csrfToken(),
       colaboradores: rows,
-      questions: rows_ques
+      questions: rows_ques,
+      indicator: rows_indi,
     });
 
   } catch (error) {
