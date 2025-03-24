11bf1bfbe5ec7a89e45abf03d10b51027049b4d9
SALV <a01707122@tec.mx>
2025-03-24 13:05:58 -0600

    [ADD] gmail send email

---
+++ backend/util/mailer.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "santialducin@gmail.com",
+    pass: "",
+  },
+});
+
+// 
+
+
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <nebulahrmsolutions@gmail.com>', // sender address
+    to: "santialducin@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
