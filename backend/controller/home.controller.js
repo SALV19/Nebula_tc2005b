60e4975f20d33376c1452ef6e581b179165ade18
yessi1327 <a01273574@tec.mx>
2025-03-24 02:17:47 -0600

    [ADD]Current Absences counter

--- backend/controller/home.controller.js
+++ backend/controller/home.controller.js
@@ -1,3 +1,22 @@
+const Requests = require("../models/home.model");
+const Events = require("../models/events.model")
+
+const absenceCount = await Requests.fetchAbsenceCount(request.session.email);
+
+exports.get_requests = async (request, response) => {
+  
+  const all_requests = await Requests.fetchDaysApproved(request.session.email).then(data => data[0]).catch(e => e);
+  const holidays = await Events.fetchEvents().then(data => data[0]).catch(error => error)
+  response.render("requests_page", {
+    selectedOption: "vacations",
+    permissions: request.session.permissions,
+    all_requests: all_requests,
+    holidays: holidays,
+    csrfToken: request.csrfToken(),
+  });
+};
+
 exports.get_home = async (request, response) => {
   response.render("home_page");
 };
+
