3edf8208625d87d24cfe47bb6cc6f737ab226bc4
Kamila <kamiii.martineeez@gmail.com>
2025-03-24 09:46:45 -0600

    [ADD] Meeting routes

--- backend/routes/followUp.routes.js
+++ backend/routes/followUp.routes.js
@@ -6,5 +6,8 @@ const follow_up_controller = require("../controller/follow_ups.controller");
 router.get('/', follow_up_controller.get_requests);
 router.post('/', follow_up_controller.post_follow_ups);
 
+router.get('/meetings', follow_up_controller.get_meeting);
+router.post('/meetings', follow_up_controller.post_meeting);
+
 
 module.exports = router;
