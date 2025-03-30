3fc964a56ec81ad68c16c9e19e62c628639646c3
SALV <a01707122@tec.mx>
2025-03-29 19:30:42 -0600

    [ADD] route_middlewares

--- backend/routes/general.routes.js
+++ backend/routes/general.routes.js
@@ -8,11 +8,13 @@ const reports_routes = require("../controller/reports.controller");
 const request_routes = require('./request.routes')
 const collabs_routes = require("./collabs.routes");
 
-router.use("/view_collabs", collabs_routes);
-router.use("/follow_ups", followUp_routes);
-router.get("/personal_info", personal_info_routes.get_personal_info);
-router.get("/reports", reports_routes.get_reports);
-router.use("/requests", request_routes);
+const permissions_middleware = require('../util/middlewares/permission.middleware')
+
+router.use("/view_collabs", permissions_middleware.view_collabs, collabs_routes);
+router.use("/follow_ups", permissions_middleware.general_permissions, followUp_routes);
+router.get("/personal_info", permissions_middleware.general_permissions, personal_info_routes.get_personal_info);
+router.get("/reports", permissions_middleware.view_reports, reports_routes.get_reports);
+router.use("/requests", permissions_middleware.general_permissions, request_routes);
 router.get("/", home_routes.get_home);
 
 module.exports = router;
