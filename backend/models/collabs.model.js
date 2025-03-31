74ef398c8f646f463680e97469eb66bbfec149fb
yessi1327 <a01273574@tec.mx>
2025-03-31 14:48:03 -0600

    [FIX]Collabs Model

--- backend/models/collabs.model.js
+++ backend/models/collabs.model.js
@@ -106,7 +106,8 @@ module.exports = class Colaborador {
         LEFT JOIN equipo e ON e.id_colaborador = c.id_colaborador
         LEFT JOIN rol r ON r.id_rol = e.id_rol
         LEFT JOIN departamento d ON d.id_departamento = e.id_departamento
-        LEFT JOIN empresa em ON em.id_empresa = d.id_empresa
+        LEFT JOIN departamento_empresa de ON de.id_departamento = d.id_departamento
+        LEFT JOIN empresa em ON em.id_empresa = de.id_empresa
         LEFT JOIN fa ON fa.id_colaborador = c.id_colaborador
         GROUP BY c.id_colaborador, c.nombre, c.apellidos, 
                 c.fechaNacimiento, c.telefono, c.puesto, c.email, 
