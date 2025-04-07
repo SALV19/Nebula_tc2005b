98ee5c7c46b30ea2257c40ed32bbaecbfd17fb88
SALV <a01707122@tec.mx>
2025-04-07 12:42:50 -0600

    [ADD] create Map: company => [departments]

--- backend/models/departamento.model.js
+++ backend/models/departamento.model.js
@@ -7,8 +7,13 @@ module.exports = class Departamento {
         }
 
     static fetchAllDep() {
-        return db.execute(`SELECT DISTINCT id_departamento, nombre_departamento FROM departamento
-                            ORDER BY nombre_departamento ASC`);
+        return db.execute(`SELECT DISTINCT e.nombre_empresa, e.id_empresa, d.id_departamento, d.nombre_departamento
+                            FROM departamento d
+                            INNER JOIN departamento_empresa de
+                            ON de.id_departamento = d.id_departamento
+                            INNER JOIN empresa e
+                            ON e.id_empresa = de.id_empresa
+                            ORDER BY nombre_empresa ASC;`);
     }
     
 };
