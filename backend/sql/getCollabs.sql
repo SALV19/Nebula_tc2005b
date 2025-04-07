01d0f2daedecbaa5fcb1f892f54b8cc8f6006ed5
SALV <a01707122@tec.mx>
2025-04-06 21:17:00 -0600

    [ADD] consult innactive_collabs

--- backend/sql/getCollabs.sql
+++ backend/sql/getCollabs.sql
@@ -90,3 +90,35 @@ SELECT  c.id_colaborador, c.nombre, c.apellidos,
                 c.modalidad, c.foto, c.curp, c.rfc, c.estado,
                 d.nombre_departamento
         ORDER BY c.nombre ASC;
+
+
+SELECT  c.id_colaborador, c.nombre, c.apellidos, 
+        c.fechaNacimiento, c.telefono, c.puesto, c.email, 
+        c.fechaIngreso, c.fechaSalida, c.ubicacion, 
+        c.modalidad, c.foto, c.curp, c.rfc, c.estado,
+        d.nombre_departamento, em.nombre_empresa,
+        r.tipo_rol,
+        COUNT(fa.id_fa) AS FaltasAdministrativas
+        FROM colaborador c
+        LEFT JOIN equipo e ON e.id_colaborador = c.id_colaborador
+        LEFT JOIN rol r ON r.id_rol = e.id_rol
+        LEFT JOIN departamento d ON d.id_departamento = e.id_departamento
+        LEFT JOIN departamento_empresa de ON de.id_departamento = d.id_departamento
+        LEFT JOIN empresa em ON em.id_empresa = de.id_empresa
+        LEFT JOIN fa ON fa.id_colaborador = c.id_colaborador
+        WHERE c.estado = 1
+        AND d.id_departamento = (
+          SELECT d.id_departamento
+          FROM departamento d
+          INNER JOIN equipo e
+            ON e.id_departamento = d.id_departamento
+          INNER JOIN colaborador c
+            ON c.id_colaborador = e.id_colaborador
+          WHERE c.email = "santialducin@gmail.com"
+        )
+        GROUP BY c.id_colaborador, c.nombre, c.apellidos, 
+                c.fechaNacimiento, c.telefono, c.puesto, c.email, 
+                c.fechaIngreso, c.fechaSalida, c.ubicacion, 
+                c.modalidad, c.foto, c.curp, c.rfc, c.estado,
+                d.nombre_departamento
+        ORDER BY c.nombre ASC
