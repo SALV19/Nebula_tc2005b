58418a779ca17929fbc0b658384a85848b003f2b
yessi1327 <a01273574@tec.mx>
2025-04-14 18:31:23 -0600

    [ADD] Routes logic and controller uploadFA function

--- backend/routes/collabs.routes.js
+++ backend/routes/collabs.routes.js
@@ -10,5 +10,6 @@ router.post('/Active', collabs_controller.get_collabs_info)
 router.post('/Inactive', collabs_controller.get_inactive)
 router.post('/get_collab_data', collabs_controller.get_collab_data);
 router.post('/update_collab', collabs_controller.update_collab);
+router.post('/uploadFA', collabs_controller.uploadFA)
 
 module.exports = router;
