078df3ed3097526a5a214e4231b9bd28feafafe5
yessi1327 <a01273574@tec.mx>
2025-04-15 10:51:11 -0600

    [ADD] Front-end Logic for empresa departamento

--- backend/controller/reports.controller.js
+++ backend/controller/reports.controller.js
@@ -15,6 +15,19 @@ exports.get_reports = async(request, response) => {
       return await Departamento.fetch(e.id_empresa);
     })
   ); 
+
+  let departamentos_empresas = departamento.map((d) => {
+    return d[0].reduce((acc, val) => {
+      if (!Object.keys(acc).length) {
+        acc = {[val.nombre_empresa]: [val.nombre_departamento]}
+      }
+      else {
+        acc = {[val.nombre_empresa]: [...acc[val.nombre_empresa], val.nombre_departamento]}
+      }
+      return acc
+    }, {})
+  })
+
   const depa = departamentos_empresas.reduce((a, v) => {
     if (!a[Object.keys(v)[0]]) {
       return {...a, [Object.keys(v)[0]]: Object.values(v)[0]}
