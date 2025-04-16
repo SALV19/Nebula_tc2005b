b5173401e6ccd11fcd290520a65dd4cb50b56a0f
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2025-04-15 18:38:01 -0600

    [FIX] merge conflict with reports!

--- backend/controller/reports.controller.js
+++ backend/controller/reports.controller.js
@@ -1,5 +1,43 @@
-exports.get_reports = (request, response) => {
-    response.render("reports", {
+const Colaborador = require("../models/collabs.model");
+const Departamento = require("../models/departamento.model");
+const Empresa = require("../models/empresa.model");
+const Reports = require("../models/reports.model");
+
+
+exports.get_reports = async(request, response) => {
+
+  const [collabs] = await Colaborador.fetchAllCompleteName()
+
+  const [empresa] = await Empresa.fetchAllEmp()
+  
+  const departamento = await Promise.all(
+    empresa.map(async (e) => {
+      return await Departamento.fetch(e.id_empresa);
+    })
+  ); 
+
+  let departamentos_empresas = departamento.map((d) => {
+    return d[0].reduce((acc, val) => {
+      if (!Object.keys(acc).length) {
+        acc = {[val.nombre_empresa]: [val.nombre_departamento]}
+      }
+      else {
+        acc = {[val.nombre_empresa]: [...acc[val.nombre_empresa], val.nombre_departamento]}
+      }
+      return acc
+    }, {})
+  })
+
+  const depa = departamentos_empresas.reduce((a, v) => {
+    if (!a[Object.keys(v)[0]]) {
+      return {...a, [Object.keys(v)[0]]: Object.values(v)[0]}
+    }
+    return {...a, [Object.keys(v)[0]]: [...a[Object.keys(v)[0]], Object.values(v)[0]]}
+  }, {})
+  
+  console.log(depa)
+
+    response.render("reports",{
       permissions: request.session.permissions,
       csrfToken: request.csrfToken(),
     });
