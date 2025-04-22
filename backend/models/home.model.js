ada2d8d561d131c3398dd009b104e200fdb8e96f
Alejandra Arredondo <103458261+alearredondo09@users.noreply.github.com>
2025-04-21 19:34:59 -0600

    Ale/fix calendar
    * [FIX] sequence diagram
    
    * [FIX] sequence diagram
    
    * [FIX] requests and calendar pagination
    
    * [FIX] calendar pop-up
    
    * [FIX] home css
    
    * [FIX] metric_counter

--- backend/models/home.model.js
+++ backend/models/home.model.js
@@ -95,7 +95,7 @@ module.exports = class Requests {
     }
 
     static async metric_month(){
-        const month = await db.execute(`SELECT 
+        const counter = await db.execute(`SELECT 
             ( 
                 (SELECT COUNT(*) 
                 FROM colaborador c 
@@ -108,10 +108,10 @@ module.exports = class Requests {
                     AND (fechaSalida IS NULL OR fechaSalida > CURRENT_DATE - INTERVAL 1 MONTH))
             ) * 100 AS indice_rotacion;
         `)
-        return month[0];
+        return counter[0];
     }
     static async metric_trimester(){
-        const trimester = await db.execute(`
+        const counter = await db.execute(`
             SELECT 
                 ( 
                     (SELECT COUNT(*) 
@@ -125,11 +125,11 @@ module.exports = class Requests {
                     AND (fechaSalida IS NULL OR fechaSalida > CURRENT_DATE - INTERVAL 3 MONTH))
                 ) * 100 AS indice_rotacion;
             `)
-        return trimester[0];
+        return counter[0];
     }
 
     static async metric_semester(){
-        const semester = await db.execute(`
+        const counter = await db.execute(`
             SELECT 
             ( 
                 (SELECT COUNT(*) 
@@ -143,10 +143,10 @@ module.exports = class Requests {
                 AND (fechaSalida IS NULL OR fechaSalida > CURRENT_DATE - INTERVAL 6 MONTH))
             ) * 100 AS indice_rotacion;
         `)        
-        return semester[0];                                      
+        return counter[0];                                      
     }
     static async metric_anually(){
-        const anual = await db.execute(`
+        const counter = await db.execute(`
             SELECT 
             ( 
                 (SELECT COUNT(*) 
@@ -160,7 +160,7 @@ module.exports = class Requests {
                 AND (fechaSalida IS NULL OR fechaSalida > CURRENT_DATE - INTERVAL 12 MONTH))
             ) * 100 AS indice_rotacion;
         `)
-        return anual[0];
+        return counter[0];
     }
 
     static async h_Rate_M(){
