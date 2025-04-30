f19ae079479e716822dfd49dfa1c599eb4ac7178
Santiago Alducin Villaseñor <145412063+SALV19@users.noreply.github.com>
2025-04-29 19:11:59 -0600

    [FIX] merge conflicts / GoogleCallback
    * [FIX] recover_password / WHEN email not in DB -> redirect to 401
    
    * [FIX] user not found error message
    
    * [FIX] fixed email in consult requests
    
    * [FIX] error message email already registered
    
    * [FIX] arrows and reason-box
    
    * [FIX] calendar event aesthetic / [FIX] broken consult collabs
    
    * [FIX] add collab form
    
    * [FIX] requests home page css
    
    * [FIX] calendar css
    
    * [FIX] download button id
    
    * [FIX] select require value before submit
    
    * [FIX] upload fault evidence button
    
    * [FIX] overwrite button upload when file already up
    
    * [FIX] requests leader id
    
    * [FIX] upload fault evidence
    
    * [FIX] company colab name
    
    * [FIX] fetch requests
    
    * [FIX] pagination / [ADD] no data -> view_collabs
    
    * [FIX] reload faults when new fault is added / [FIX] comany on collabs-page / [FIX] innactive collabs kept out
    
    * [ADD] exit day on innactive collabs
    
    * [FIX] add exit day when registering third fault to collab
    
    * [FIX] error link not in query, reload page, add date to exit day the moment it reaches 3 faults
    
    * [FIX] download reports / not being cut / hide update button
    
    * [FIX] update / add abscence request
    
    * [FIX] english placeholder
    
    * [FIX] chart title reports
    
    * [FIX] Background
    
    * [FIX] absence days in view collabs with same label as home
    
    * [ADD] collab sign w/ google when not in DB
    
    * [FIX] broken log-in
    
    * [FIX] correct create fault form optional assistants entry
    
    * [DELETE] unnecesary console.logs
    
    * [FIX] collabs overflow
    
    * [ADD] collabs manual
    
    * [FIX] get requests colabAprobador
    
    * [DELETE] console.log
    
    * [FIX] get vacations and abscences requests colla aprobador
    
    * [TEST] html-pdf
    
    * [FIX] evidence button on own absences / vacation requests
    
    * [FIX] phantom to generate pdfs
    
    * [FIX] phantom path 2
    
    * [FIX] new pdf generation method
    
    * [FIX] change style report pdf
    
    * [FIX] puppeteer browser-path
    
    * [FIX] chromium
    
    * [FIX] typo
    
    * [FIX] Google callbacll
    
    ---------
    
    Co-authored-by: Ale Arredondo <alejandrarredondo09@gmail.com>
    Co-authored-by: yessi1327 <a01273574@tec.mx>

--- backend/util/google_auth.js
+++ backend/util/google_auth.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/log_in/google/callback",
+      callbackURL: process.env.CALL_BACK ?? "http://localhost:3000/log_in/google/callback",
       scope: [...scope_def],
       accessType: "offline",
       prompt: "consent",
